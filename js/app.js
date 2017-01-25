7ed94d116b531de11f0889d6e3363fd4418574dd
leowolf749 <leowolf749@gmail.com>
2017-01-24 22:09:43 -0500

    getting data from the chats server. Now need to render it when i get it.

--- js/app.js
+++ js/app.js
@@ -5,18 +5,21 @@ const MessageView = require('./views/message');
 window.addEventListener('load', function () {
     // const message = new MessageModel();
     const list = new MessageList();
+    list.fetch();
+    
 
     const view = new MessageView({
         el: document.querySelector('body'),
         model: list,
     });
+
+
     view.render();
 });
 
 Backbone.sync = function (method, model) {
     if (method === 'create' || method === 'update') {
-        // console.log('frum')
-        // console.log(model.get('from'));
+
         const request = new XMLHttpRequest();
         request.open('POST', 'http://api.queencityiron.com/chats');
 
@@ -37,14 +40,15 @@ Backbone.sync = function (method, model) {
 
             for (let i = 0; i < response.chats.length; i++) {
                 // 
-                // msg = new ChatModel(); <=== make a new ChatModel for each of response.chats
-                // msg.set('from', response.chats[i].from);
-                // model.add(that ChatModel from above)
+                msg = new MessageList(); 
+                msg.set('from', response.chats[i].from);
+                msg.set('message', response.chats[i].message);
+                model.add(msg);
             }
-            // model.set('name', response.name);
-            // model.set('attendees', response.attendees);
-            // model.set('when', response.when);
-            // model.trigger('change');
+            model.set('from', response.from);
+            model.set('message', response.message);
+            model.trigger('change');
+            console.log(response);
         });
         request.send();
     }
